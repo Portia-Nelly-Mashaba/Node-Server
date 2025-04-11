72b7f84db51359b8e63fc1f1a2f69e6020facf72
pnpnelly@gmail.com <Portia-Nelly-Mashaba>
2025-04-11 08:55:55 +0200

    added ReadMe file and updated server script

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const port = 3000;
 
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the Basic Node.js Server!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Route not found.');
+  }
 });
 
 
